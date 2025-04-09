109e2bb4963747e048334b0dc3622d92c80d235d
HeedlessSoap325 <mathisjulian123@gmail.com>
2025-04-09 21:08:07 +0200

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import CustomWebcam from "./components/CustomWebcam.jsx";
+
+export default function App() {
+  return (
+      <div className={"App-Container"}>
+          <CustomWebcam />
+      </div>
+  );
+}
