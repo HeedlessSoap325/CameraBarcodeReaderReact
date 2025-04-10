5229cbb4fa4b081389c1f3b4d1f455d3abc5ec62
HeedlessSoap325 <mathisjulian123@gmail.com>
2025-04-10 13:24:55 +0200

    Passe App.jsx an, um den Decoder.jsx zu beinhalten

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
 import CustomWebcam from "./components/CustomWebcam.jsx";
+import Decoder from "./components/Decoder.jsx";
+import {useState} from "react";
 
 export default function App() {
+    const [barcode, setBarcode] = useState("");
+
   return (
       <div className={"App-Container"}>
-          <CustomWebcam />
+          <CustomWebcam setBarCode={setBarcode} />
+          <Decoder barcode={barcode} />
       </div>
   );
 }
